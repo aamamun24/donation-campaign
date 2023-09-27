8255ee21cb22aa25cc3e00542cf4243e250fc15d
aamamun24 <bdmamaun100@gmail.com>
2023-09-27 10:51:51 +0600

    donation page's button functionality completed

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -2,21 +2,35 @@ import { useEffect, useState } from "react";
 import DonationCard from "./DonationCard";
 
 const Donation = () => {
-    const [cards, setCards] = useState([])
+    const [cards, setCards] = useState([]);
+    const [noDonation, setNoDonation] = useState(false);
+    const [seeAll, setSeeAll] = useState(false);
 
     useEffect(() => {
         const donations = JSON.parse(localStorage.getItem('donation'));
-        setCards(donations)
+        if (donations) {
+            setCards(donations)
+        }
+        else {
+            setNoDonation("You have not donate yet")
+        }
     }, [])
 
     return (
-        <div className="mt-10 md:mt-20 mb-16 md:mb-32">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                {
-                    cards.map(card => <DonationCard key={card.id} card={card}></DonationCard>)
-                }
-            </div>
-            <button className="text-white font-semibold bg-[#009444] py-4 px-7 rounded-lg mt-10 block mx-auto">See All</button>
+        <div>
+            {
+                noDonation ? <h2 className="text-3xl text-center font-semibold flex items-center justify-center h-[75vh]">{noDonation}</h2> :
+                    <div className="mt-10 md:mt-20 mb-16 md:mb-32">
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                            {
+                                seeAll ? cards.map(card => <DonationCard key={card.id} card={card}></DonationCard>)
+                                    :
+                                    cards.slice(0, 4).map(card => <DonationCard key={card.id} card={card}></DonationCard>)
+                            }
+                        </div>
+                        {cards.length > 4 && <button onClick={() => setSeeAll(!seeAll)} className="text-white font-semibold bg-[#009444] py-4 px-7 rounded-lg mt-10 block mx-auto">{seeAll ? "See Less" : "See All"}</button>}
+                    </div>
+            }
         </div>
     );
 };
--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
         <div>
             <h2>Oops!!</h2>
+            <Link to="/">
+                <button className='bg-green-400 text-white text-lg font-semibold py-2 px-4 rounded mt-5'>Go Back Home</button>
+            </Link>
         </div>
     );
 };
